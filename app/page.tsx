f28dab6178a06a704b094cd4a81342f7b0faefef
Aurelio <aurelioarcabascio@gmail.com>
2023-11-26 10:57:31 -0500

    fixed hydration error and added random data to graph

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+'use client'
 import PriceChart from '@/components/PriceChart'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <PriceChart />
-    </main>
+    </div>
   )
 }
--- components/PriceChart.tsx
+++ components/PriceChart.tsx
@@ -1,24 +1,47 @@
-'use client'
-import React from 'react'
-import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
+import { FC, useEffect, useState } from 'react'
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
 
-const PriceChart: React.FC = () => {
-    const data = [
-        { date: "11/01/2023", price: '315200' },
-        { date: "11/02/2023", price: '323100' },
-        { date: "11/03/2023", price: '317200' },
-        { date: "11/04/2023", price: '298800' },
-        { date: "11/05/2023", price: '309200' },
-        { date: "11/06/2023", price: '300000' },
-    ]
+type Prices = {
+    date: String,
+    price: Number
+}
+
+const generateData = (): Array<Prices> => {
+    let res: Array<Prices> = []
+
+    for (let i = 0; i < 10; i++) {
+        let num = Math.floor(Math.random() * (300000 - 100000) + 100000)
+        if (i % 2) {
+            res.push({ date: "12:00 AM", price: num })
+        } else {
+            res.push({ date: "6:00 PM", price: num })
+        }
+    }
+
+    return res
+}
+
+export const useIsServerSide = () => {
+    const [isServerSide, setIsServerSide] = useState(true)
+
+    useEffect(() => {
+        setIsServerSide(false)
+    }, [setIsServerSide])
+
+    return isServerSide
+}
+
+const PriceChart: FC = () => {
+    const isServerSide = useIsServerSide()
+    const data = generateData()
+    if (isServerSide) return null
 
     return (
         <div>
             <LineChart width={600} height={300} data={data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="date" />
-                <YAxis />
+                <YAxis domain={[ 100000, 300000 ]} />
                 <Tooltip />
                 <Line type="monotone" dataKey="price" stroke="#8884d8" />
             </LineChart>
