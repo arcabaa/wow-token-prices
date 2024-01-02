425ef2ead1cee1ee7b6124abc01249609b2ba87f
Aurelio <aurelioarcabascio@gmail.com>
2024-01-02 12:15:25 -0500

    Styling done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 'use client'
 import PriceChart from '@/components/PriceChart'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PriceChart />
+    <div className="flex flex-col items-center p-24">
+      <Tabs defaultValue='NA' className='flex flex-col items-center'>
+        <TabsList>
+          <TabsTrigger value="NA">North America</TabsTrigger>
+          <TabsTrigger value="EU">Europe</TabsTrigger>
+        </TabsList>
+        <TabsContent value="NA">
+          <PriceChart />
+        </TabsContent>
+        <TabsContent value="EU">
+          <PriceChart />
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
--- components/PriceChart.tsx
+++ components/PriceChart.tsx
@@ -1,23 +1,53 @@
 import { FC, useEffect, useState } from 'react'
 import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
+import '../app/globals.css'
 
 type PriceData = {
     date: String,
     price: Number
 }
 
+const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+        const dateAndTimeSplit = label.split(' ')
+        const date = dateAndTimeSplit[0]
+        const time = `${dateAndTimeSplit[1]} ${dateAndTimeSplit[2]}`
+
+        return (
+        <div className="custom-tooltip">
+            <p>{`Date: ${date}`}</p>
+            <p>{`Time: ${time}`}</p>
+            <p className="label">{`Price: ${payload[0].value}g`}</p>
+        </div>
+        );
+    }
+  
+    return null;
+  };
+
 const generateData = (): Array<PriceData> => {
     let res: Array<PriceData> = []
 
-    for (let i = 0; i < 10; i++) {
-        let num = Math.floor(Math.random() * (300000 - 100000) + 100000)
-        if (i % 2) {
-            res.push({ date: "12:00 AM", price: num })
-        } else {
-            res.push({ date: "6:00 PM", price: num })
-        }
-    }
-
+    let num: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "12/30/23 12:00 PM", price: num })
+    let num2: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "12/30/23 3:00 PM", price: num2 })
+    let num3: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "12/30/23 6:00 PM", price: num3 })
+    let num4: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "12/30/23 9:00 PM", price: num4 })
+    let num5: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "12/31/23 12:00 PM",price: num5 })
+    let num6: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "12/31/23 3:00 PM", price: num6 })
+    let num7: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "12/31/23 6:00 PM", price: num7 })
+    let num8: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "12/31/23 9:00 PM", price: num8 })
+    let num9: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "01/01/24 12:00 PM", price: num9 })
+    let num10: Number = Math.floor(Math.random() * (300000 - 100000) + 100000)
+    res.push({ date: "01/01/24 3:00 PM", price: num10 })
     return res
 }
 
@@ -32,18 +62,23 @@ export const useIsServerSide = (): boolean => {
 }
 
 const PriceChart: FC = () => {
+    const [screenWidth, setScreenWidth] = useState(0)
+
+    useEffect(() => {
+        setScreenWidth(window.innerWidth)
+    }, [])
+
     const isServerSide = useIsServerSide()
     const data = generateData()
-    if (isServerSide) return null
 
+    if (isServerSide) return null
     return (
-        <div>
-            <LineChart width={600} height={300} data={data}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <XAxis dataKey="date" />
-                <YAxis domain={[ 100000, 300000 ]} />
-                <Tooltip />
-                <Line type="monotone" dataKey="price" stroke="#8884d8" />
+        <div className="mt-20">
+            <LineChart width={screenWidth - 400} height={400} data={data}>
+                <XAxis dataKey="date" stroke="#b1aa9e" />
+                <YAxis stroke='#b1aa9e' />
+                <Tooltip content={<CustomTooltip />} />
+                <Line type="monotone" dataKey='price' stroke="#b1aa9e" dot={false} />
             </LineChart>
         </div>
     )
